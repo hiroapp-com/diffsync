75a707218ba714d2c430dab37c761143bd85df3a
Flo Lauber <fl-dev@qatfy.at>
2014-03-20 13:22:20 -0600

    NoteValue now implements ResourceValue

--- note_value.go
+++ note_value.go
@@ -1,7 +1,7 @@
 package diffsync 
 
 import (
-//    DMP "github.com/sergi/go-diff/diffmatchpatch"
+    DMP "github.com/sergi/go-diff/diffmatchpatch"
 )
 
 type NoteValue string
@@ -11,20 +11,41 @@ func NewNoteValue(text string) NoteValue {
 }
 
 
-//func (note *NoteValue) ApplyDelta(delta Delta, notify chan<- Event)  (Patch, error) {
-//    strnote := string(*note)
-//    diffs, err := dmp.DiffFromDelta(strnote, delta.(string))
-//    if err != nil {
-//        return nil, err
-//    }
-//    patch := dmp.PatchMake(strnote, diffs)
-//    *note = NoteValue(dmp.DiffText2(diffs))
-//    return patch, nil
-//}
-//
-//func (note *NoteValue) ApplyPatch(patch Patch) (patched_note ResourceValue, changed bool, err error) {
-//    patched_str, _ := dmp.PatchApply([]DMP.Patch{patch.(DMP.Patch)}, string(*note))
-//    changed = string(*note) != patched_str
-//    patched := NoteValue(patched_str)
-//    return &patched, changed, nil 
-//}
+
+
+//note maybe make notify a global chan
+func (note *NoteValue) ApplyDelta(delta Delta)  (Patch, error) {
+    original := string(*note)
+    diffs, err := dmp.DiffFromDelta(original, delta.(string))
+    if err != nil {
+        return Patch{}, err
+    }
+    patch := dmp.PatchMake(original, diffs)
+    *note = NoteValue(dmp.DiffText2(diffs))
+    if original == string(*note) {
+        return Patch{}, nil
+    }
+    return Patch{val: patch}, nil
+}
+
+// maybe notify should be a global chan
+func (note *NoteValue) ApplyPatch(patch Patch, notify chan<- Event) (changed bool, err error) {
+    patched_str, _ := dmp.PatchApply([]DMP.Patch{patch.val.(DMP.Patch)}, string(*note))
+    changed = string(*note) != patched_str
+    *note = NoteValue(patched_str)
+    // more logical patches (like meta) could send res-taint events to notify after modifying others' resources (e.g. folio)
+    return changed, nil 
+}
+
+func (note *NoteValue) GetDelta(other ResourceValue) (Delta, error) {
+    return "", nil
+}
+
+func (note *NoteValue) MarshalJSON() ([]byte, error) {
+    return []byte(*note), nil
+}
+
+func (note *NoteValue) UnmarshalJSON(from []byte) error {
+    *note = NoteValue(from)
+    return nil
+}
