b28861d70f434fef684ac596de17f18e8d45abcb
Flo Lauber <fl-dev@qatfy.at>
2014-03-21 18:55:15 -0600

    add first draft of `meta` resource

---
+++ meta_value.go
@@ -0,0 +1,42 @@
+package diffsync
+
+import (
+	"errors"
+	"time"
+)
+
+type MetaValue struct {
+	title         string
+	updated_at    *time.Time
+	updated_by    string
+	collaborators []string
+	seen_by       []string
+}
+
+//note maybe make notify a global chan
+func (note *MetaValue) ApplyDelta(delta Delta) (Patch, error) {
+	return Patch{}, errors.New("Not implemented")
+}
+
+// maybe notify should be a global chan
+func (note *MetaValue) ApplyPatch(patch Patch, notify chan<- Event) (changed bool, err error) {
+	return false, errors.New("Not implemented")
+}
+
+func (note *MetaValue) GetDelta(other ResourceValue) (Delta, error) {
+	return "", errors.New("Not implemented")
+}
+
+func (note *MetaValue) MarshalJSON() ([]byte, error) {
+	return []byte{}, errors.New("Not implemented")
+
+}
+
+func (note *MetaValue) UnmarshalJSON(from []byte) error {
+	return errors.New("Not implemented")
+}
+
+func (meta *MetaValue) CloneValue() ResourceValue {
+	x := *meta
+	return &x
+}
