607a825908644103cecaa536ac485c7c278ac020
Flo Lauber <fl-dev@qatfy.at>
2014-03-26 13:20:09 -0600

    minor Resource refactoring

--- resource.go
+++ resource.go
@@ -13,28 +13,44 @@ type ResourceValue interface {
 	CloneValue() ResourceValue
 	json.Marshaler
 	json.Unmarshaler
+	fmt.Stringer
 }
 
+// todo(refactore) export fields
 type Resource struct {
 	kind string
 	id   string
 	ResourceValue
 }
 
-func (res *Resource) CloneEmpty() *Resource {
-	return &Resource{kind: res.kind, id: res.id}
+func NewResource(kind, id string) Resource {
+	return Resource{kind: kind, id: id, ResourceValue: NilValue{}}
 }
 
-func (res *Resource) StringId() string {
+func (res *Resource) CloneEmpty() Resource {
+	return Resource{kind: res.kind, id: res.id, ResourceValue: NilValue{}}
+}
+
+func (res *Resource) StringID() string {
 	return fmt.Sprintf("%s:%s", res.kind, res.id)
 }
 
-func (res *Resource) MarshalJSON() ([]byte, error) {
-	return json.Marshal(map[string]interface{}{
+func (res *Resource) String() string {
+	if res.ResourceValue != nil {
+		return fmt.Sprintf("kind: `%s`, id:`%s`", res.kind, res.id)
+	}
+	return fmt.Sprintf("kind: `%s`, id:`%s`, value:`%s`", res.kind, res.id, res.ResourceValue.String())
+}
+
+func (res Resource) MarshalJSON() ([]byte, error) {
+	data := map[string]interface{}{
 		"kind": res.kind,
 		"id":   res.id,
-		"val":  res.ResourceValue,
-	})
+	}
+	if _, is_nil := res.ResourceValue.(NilValue); !is_nil {
+		data["val"] = res.ResourceValue
+	}
+	return json.Marshal(data)
 }
 
 func (res *Resource) UnmarshalJSON(from []byte) error {
