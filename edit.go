ea771720d56bfb9e735fe0a1aa491056987fe419
Flo Lauber <fl-dev@qatfy.at>
2014-03-26 13:21:50 -0600

    helper functions for Edit

--- edit.go
+++ edit.go
@@ -17,3 +17,14 @@ type Edit struct {
 	SessionClock
 	delta Delta
 }
+
+func NewEdit(delta Delta) Edit {
+	return Edit{SessionClock{}, delta}
+}
+
+func (edit Edit) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]interface{}{
+		"clock": edit.SessionClock,
+		"delta": edit.delta,
+	})
+}
