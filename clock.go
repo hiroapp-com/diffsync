3f03127509eec3ed3c630b8d9bd54f6a93a39472
Flo Lauber <fl-dev@qatfy.at>
2014-03-27 20:28:49 -0600

    export clock fields

--- clock.go
+++ clock.go
@@ -1,8 +1,13 @@
 package diffsync
 
 import (
-	"encoding/json"
+	"log"
+	//"encoding/json"
 	"fmt"
+)
+
+var (
+	_ = log.Print
 )
 
 type Versioned interface {
@@ -24,36 +29,39 @@ func (c VersionsDivergedError) Error() string {
 }
 
 type SessionClock struct {
-	cv, sv, bv int64
+	CV int64 `json:"cv"`
+	SV int64 `json:"sv"`
+	BV int64 `json:"bv,omitempty"`
 }
 
 func (clock SessionClock) Clone() SessionClock {
-	return SessionClock{clock.cv, clock.sv, clock.bv}
+	return SessionClock{clock.CV, clock.SV, clock.BV}
 }
 
 func (clock SessionClock) Cv() int64 {
-	return clock.cv
+	return clock.CV
 }
 func (clock *SessionClock) IncCv() {
-	clock.cv++
+	clock.CV++
 }
 func (clock SessionClock) Sv() int64 {
-	return clock.sv
+	return clock.SV
 }
 func (clock *SessionClock) IncSv() {
-	clock.sv++
+	clock.SV++
 }
 
 func (clock *SessionClock) Checkpoint() {
-	clock.bv = clock.sv
+	clock.BV = clock.SV
 }
 
 func (clock *SessionClock) SyncSvWith(other Versioned, rb Rollbacker) error {
-	if clock.sv != other.Sv() {
-		if clock.bv != other.Sv() {
+	if clock.SV != other.Sv() {
+		if clock.BV != other.Sv() {
 			return VersionsDivergedError{*clock, other}
 		}
-		clock.sv = clock.bv
+		log.Println("sessionclock: SV mismatch, rolling back backup")
+		clock.SV = clock.BV
 		rb.Rollback()
 	}
 	return nil
@@ -61,34 +69,16 @@ func (clock *SessionClock) SyncSvWith(other Versioned, rb Rollbacker) error {
 
 func (clock SessionClock) Ack(pending Versioned) bool {
 	// Return whether clock acks other
-	return clock.sv >= pending.Sv()
+	return clock.SV >= pending.Sv()
 }
 func (clock SessionClock) CheckCV(other Versioned) (is_dupe bool, err error) {
 	switch {
-	case clock.cv == other.Cv():
+	case clock.CV == other.Cv():
 		return false, nil
-	case clock.cv > other.Cv():
+	case clock.CV > other.Cv():
 		return true, nil
-	case clock.cv < other.Cv():
+	case clock.CV < other.Cv():
 		return false, VersionsDivergedError{clock, other}
 	}
 	return false, nil
-}
-
-func (clock SessionClock) MarshalJSON() ([]byte, error) {
-	return json.Marshal(map[string]int64{
-		"cv": clock.cv,
-		"sv": clock.sv,
-		"bv": clock.bv,
-	})
-}
-
-func (clock *SessionClock) UnmarshalJSON(from []byte) error {
-	vals := make(map[string]int64)
-	json.Unmarshal(from, vals)
-	*clock = SessionClock{cv: vals["cv"],
-		sv: vals["sv"],
-		bv: vals["bv"],
-	}
-	return nil
 }
