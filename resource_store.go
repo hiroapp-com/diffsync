b064a98cd2ab9826decd49883c4e63e3c5ef904f
Flo Lauber <fl-dev@qatfy.at>
2014-06-21 15:36:16 -0600

    extend and refactor Store

--- resource_store.go
+++ resource_store.go
@@ -12,13 +12,33 @@ var (
 )
 
 type Store struct {
-	kind    string
-	backend StoreBackend
-	notify  chan<- Event
+	kind     string
+	backends map[string]StoreBackend
+	backend  StoreBackend
+	notify   chan<- Event
 }
 
-func NewStore(kind string, backend StoreBackend, notify chan<- Event) *Store {
-	return &Store{kind: kind, backend: backend, notify: notify}
+func (store *Store) OpenNew(kind string) *Store {
+	return &Store{kind: kind, backends: store.backends, backend: store.backends[kind], notify: store.notify}
+}
+
+func NewStore(kind string, backends map[string]StoreBackend, notify chan<- Event) *Store {
+	return &Store{kind: kind, backends: backends, backend: backends[kind], notify: notify}
+}
+
+func (store *Store) CreateEmpty() (Resource, error) {
+	res := Resource{Kind: store.kind}
+	// create new Nil Resource in backend
+	var err error
+	res.ID, err = store.backend.Insert(nil)
+	if err != nil {
+		return Resource{}, err
+	}
+	// and now retrieve it
+	if err = store.Load(&res); err != nil {
+		return Resource{}, err
+	}
+	return res, nil
 }
 
 func (store *Store) Load(res *Resource) error {
@@ -35,9 +55,22 @@ func (store *Store) Load(res *Resource) error {
 	return nil
 }
 
-func (store *Store) NotifyReset(id string, sid string) {
+func (store *Store) LoadOrCreate(res *Resource) (created bool, err error) {
+	err = store.Load(res)
+	if _, ok := err.(NoExistError); ok {
+		newID, err := store.backend.Insert(res.Value)
+		if err != nil {
+			return false, err
+		}
+		res.ID = newID
+		created = true
+	}
+	return
+}
+
+func (store *Store) NotifyReset(id string) {
 	select {
-	case store.notify <- Event{Name: "res-reset", SID: sid, Res: Resource{Kind: store.kind, ID: id}}:
+	case store.notify <- Event{Name: "res-reset", SID: "", Res: Resource{Kind: store.kind, ID: id}}:
 		return
 	default:
 		log.Printf("store[%s]: cannot send `res-reset`, notify channel not writable.\n", store.kind)
@@ -53,20 +86,20 @@ func (store *Store) NotifyTaint(id string, sid string) {
 	}
 }
 
-func (store *Store) Patch(res *Resource, patch Patcher, sid string) error {
+func (store *Store) Patch(res *Resource, patch Patcher, sid string) (bool, error) {
 	value, err := store.backend.Get(res.ID)
 	if err != nil {
-		return err
+		return false, err
 	}
-	newval, err := patch.Patch(value, store.notify)
+	newval, err := patch.Patch(value, store)
 	if err != nil {
-		return err
+		return false, err
 	}
 	if newval.GetDelta(value).HasChanges() {
 		if err := store.backend.Upsert(res.ID, newval); err != nil {
-			return err
+			return true, err
 		}
-		store.NotifyTaint(res.ID, sid)
+		return true, nil
 	}
-	return nil
+	return false, nil
 }
