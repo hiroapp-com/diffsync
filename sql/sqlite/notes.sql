0ccc839bc1ba9f24cae4c87f4d4768805c2da680
Flo Lauber <fl-dev@qatfy.at>
2014-11-27 16:57:29 +0800

    remove obsolete sqlite sql files

--- sql/sqlite/notes.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "notes" (
-    nid text PRIMARY KEY,
-    title text default "",
-    txt text default "",
-    sharing_token text default "",
-    created_at timestamp default NOW(),
-    created_by text default ""
-);
--- sql/sqlite/stripe_tokens.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "stripe_tokens" (
-    token text default "",
-    uid text default "",
-    seen_at timestamp
-);
-
